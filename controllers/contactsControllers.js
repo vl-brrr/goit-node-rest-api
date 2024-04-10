33c7b23a90ef50a0ef8917b048e35947cbbeda76
vl-brrr <vlada.brayko@gmail.com>
2024-04-10 18:35:29 +0300

    HM#2 done

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,92 @@
-import contactsService from "../services/contactsServices.js";
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContactById,
+} from "../services/contactsServices.js";
+import { createContactSchema } from "../schemas/contactsSchemas.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  try {
+    const contacts = await listContacts();
 
-export const getOneContact = (req, res) => {};
+    res.status(200).json({
+      msg: "success!",
+      contacts,
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const contact = await getContactById(id);
 
-export const createContact = (req, res) => {};
+    if (contact !== null) {
+      res.status(200).json({
+        msg: "success!",
+        contact,
+      });
+    } else {
+      res.status(404).json({ msg: "Not found" });
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
-export const updateContact = (req, res) => {};
+export const deleteContact = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const contact = await removeContact(id);
+
+    if (contact !== null) {
+      res.status(200).json({
+        msg: "success!",
+        contact,
+      });
+    } else {
+      res.status(404).json({ msg: "Not found" });
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+export const createContact = async (req, res) => {
+  try {
+    const { value } = createContactSchema.validate(req.body);
+
+    const contact = await addContact(value);
+    res.status(201).json({
+      msg: "success!",
+      contact,
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+export const updateContact = async (req, res) => {
+  try {
+    if (req.body.length < 1) {
+      res.status(404).json({ msg: "Body must have at least one field" });
+    }
+    const { id } = req.params;
+    const contact = await getContactById(id);
+    if (contact !== null) {
+      const updatedContact = await updateContactById(id, req.body);
+      res.status(200).json({
+        msg: "success!",
+        updatedContact,
+      });
+    } else {
+      res.status(404).json({ msg: "Not found" });
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+};
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,18 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-
-})
+  name: Joi.string().min(2).max(30).required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string()
+    .length(14)
+    .pattern(/^[(]+[0-9]{3}[)]+[ ]+[0-9]{3}[-]+[0-9]{4}$/)
+    .required(),
+});
 
 export const updateContactSchema = Joi.object({
-
-})
+  name: Joi.string().min(2).max(30),
+  email: Joi.string().email(),
+  phone: Joi.string()
+    .length(14)
+    .pattern(/^[(]+[0-9]{3}[)]+[ ]+[0-9]{3}[-]+[0-9]{4}$/),
+});
